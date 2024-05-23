9b9a227fb2da44431c6b6ac016e2114fcf760c9c
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-05-22 23:16:34 -0500

    Added person.js from Section 2

--- src/index.js
+++ src/index.js
@@ -3,15 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {firstName as myvalue,
+  duplicate as duplicate2} from './person';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+document.getElementById("app").innerHTML = '${firstName} ${age} ${duplicate{2}}';
---
+++ src/person.js
@@ -0,0 +1,5 @@
+export const firstName = "Felipe";
+export const age = 99;
+export const duplicate = (value) => value*2;
+
+// export {firstName, age, duplicate};
