c35ca0b0f7300f6f964f1936caaec1c7a70afcee
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-05-26 21:00:24 -0500

    Section 3: A solution is REACT

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-function App(){
-    return (
-        <div className="App-Header">
-            <img src={logo} className='App-logo' alt="logo" />
-            <p>
-                Edit <code>src/App.tsx</code> and save to reload
-            </p>
-            <a
-            className="App-link"
-            href=""
-            target=''
-            rel=""
-            >
-                Learn React
-            </a>
+
+function App() {
+    const [myDate, myDateUpdate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            myDateUpdate(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+});
+
+
+return (
+    <div>
+        <h3>Example React</h3>
+        <input />
+        <div>{myDate.toString()}</div>
         </div>
-    )
+    );
+
 }
+
+
+
+
+
+// function App(){
+//     return (
+//         <div className="App">
+//             <header className="App-Header">
+//             <p>
+//                 Edit <code>src/App.tsx</code> and save to reload
+//             </p>
+//             <a
+//             className="App-link"
+//             href="https://react.js.org"
+//             target='_blank'
+//             rel="noopener noreferrer"
+//             >
+//                 Learn React
+//             </a>
+//             </header>
+//         </div>
+//     )
+// }
