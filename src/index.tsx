d205898162b281ac87c37eafd9a3ce425641b4c0
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-05-28 23:53:16 -0500

    Added comment

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,11 @@ ReactDOM.render(
         <App />
     </React.StrictMode>,
     document.getElementById('root')
+    console.getElementId;
 );
 
 // if you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more
+// Adding a comment
 reportWebVitals();
