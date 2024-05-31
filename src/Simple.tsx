167d78cc7b19aa8a265a1a67061ad19a777f5b62
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-05-30 22:32:24 -0500

    Section  3: Making a simple component

---
+++ src/Simple.tsx
@@ -0,0 +1,10 @@
+export default function Simple(){
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <h2>This is a subtext</h2>
+        </div>
+    )
+}
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,16 @@ import ReactDOM from 'react-dom';
 import './inde.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Simple from './simple';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Simple />
     </React.StrictMode>,
     document.getElementById('root')
-    console.getElementId;
 );
 
 // if you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more
-// Adding a comment
 reportWebVitals();
