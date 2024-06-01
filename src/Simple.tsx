8faa87d654814aa836fa43d59c22e75ce85b4066
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-05-31 22:09:03 -0500

    Added a new function in Simple.tsx for JSX

--- src/Simple.tsx
+++ src/Simple.tsx
@@ -1,10 +1,23 @@
-export default function Simple(){
+import React from "react";
+
+export default function Simple() {
     return (
-        <div>
-            <h1>Hello world</h1>
-            <h2>This is a subtext</h2>
-        </div>
+        React.createElement("div", {
+            myattribute: "myvalue"
+        }, React.createElement("h1", null, "Hello World 2"),
+            React.createElement("h2", null, "This is a subtext"))
     )
 }
+
+
+// export default function Simple(){
+//     return (
+//         <div>
+//             <h1>Hello world</h1>
+//             <h2>This is a subtext</h2>
+//             <h3>A message for Simple function</h3>
+//         </div>
+//     )
+// }
 
 
