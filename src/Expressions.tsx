bb9dbd3847950545f19f3f90c06649f4a38a1883
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-06-01 22:29:40 -0500

    Expressions of JSX

--- src/Expressions.tsx
+++ src/Expressions.tsx
@@ -1,10 +1,15 @@
 export default function Expressions(){
     const subtext = 'This is my subtext'
+    const reactLogoURL = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AReact-icon.svg&psig=AOvVaw16xcI9R7gjeAd-rkI-QOj1&ust=1717385241619000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCIi2sNv8u4YDFQAAAAAdAAAAABAE';
+    
+    const duplicate = (value: number) => value * 2;
+
     return (
         <>
             <h1>Expressions example</h1>
             <h2>{subtext.toUpperCase()}</h2>
-            <img alt="react logo" />
+            <h3>The double of 3 is equal to {duplicate(3)}</h3>
+            <img src={reactLogoURL} alt="react logo" />
         </>
     )
 }
